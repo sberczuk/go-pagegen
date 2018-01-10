cf756eddc139e2b3323a460898cbbccbc5bbbb32
Steve Berczuk <sberczuk@fitbit.com>
2018-01-09 19:49:30 -0500

    Skeleton of a parsing tool using an HTML parser. Next step is to parse to a basic JSON Structure.

--- events-java/src/main/java/HtmlParser.java
+++ events-java/src/main/java/HtmlParser.java
@@ -10,16 +10,39 @@ public class HtmlParser {
     public static void main(String[] args) throws IOException {
         File input = new File("/Users/sberczuk/code/personal/go-pagegen/events/events.html");
         Document doc = Jsoup.parse(input, "UTF-8", "http://example.com/");
+        HtmlParser htmlParser = new HtmlParser();
+        htmlParser.processEvents(doc);
+    }
+
+    public void processEvents(Document doc){
         Elements rows = doc.getElementsByTag("tr");
         for (Element row : rows){
-            System.out.println(row);
-            if(row.children().size() >2 ) {
-                System.out.println(">" + row.child(0));
-                System.out.println(">>" + row.child(1));
-                System.out.println(">>" + row.child(2));
+            //System.out.println(row);
+            if(row.children().size() ==3 ) {
+                processEventRow(row);
             } else{
                 System.out.println("NO");
             }
         }
+    }
+
+    private  void processEventRow(Element row) {
+        Element venueTD = row.child(0);
+        processVenueRow(venueTD);
+        Element dateTD = row.child(1);
+        processDateRow(dateTD);
+        Element descriptionTD = row.child(2);
+        processDescription(descriptionTD);
+    }
+
+    private void processDescription(Element descriptionTD) {
+    }
+
+    private void processDateRow(Element dateTD) {
+        
+    }
+
+    private  void processVenueRow(Element venueTD) {
+
     }
 }
