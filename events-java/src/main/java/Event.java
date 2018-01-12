4124c2ed698502178572bf3c75928786e51643dd
Steve Berczuk <sberczuk@fitbit.com>
2018-01-12 17:16:05 -0500

    Checkpoint: JSON Creation

---
+++ events-java/src/main/java/Event.java
@@ -0,0 +1,47 @@
+public class Event {
+    private String venue;
+    private String venueLink="";
+    private String date="";
+    private String description="";
+    private String documentLink="";
+
+    public String getVenue() {
+        return venue;
+    }
+
+    public void setVenue(String venue) {
+        this.venue = venue;
+    }
+
+    public String getVenueLink() {
+        return venueLink;
+    }
+
+    public void setVenueLink(String venueLink) {
+        this.venueLink = venueLink;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDocumentLink() {
+        return documentLink;
+    }
+
+    public void setDocumentLink(String documentLink) {
+        this.documentLink = documentLink;
+    }
+}
--- events-java/src/main/java/HtmlParser.java
+++ events-java/src/main/java/HtmlParser.java
@@ -3,36 +3,64 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import javax.json.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HtmlParser {
     public static void main(String[] args) throws IOException {
         File input = new File("/Users/sberczuk/code/personal/go-pagegen/events/events.html");
         Document doc = Jsoup.parse(input, "UTF-8", "http://example.com/");
         HtmlParser htmlParser = new HtmlParser();
-        htmlParser.processEvents(doc);
+        JsonArray jsonObject = htmlParser.processEvents(doc);
+        System.out.println(jsonObject);
     }
 
-    public void processEvents(Document doc){
+    public JsonArray processEvents(Document doc){
         Elements rows = doc.getElementsByTag("tr");
+        List<Event> events = new ArrayList<>();
         for (Element row : rows){
-            //System.out.println(row);
+             //System.out.println(row);
             if(row.children().size() ==3 ) {
-                processEventRow(row);
+                Event event = processEventRow(row);
+                events.add(event);
+
             } else{
                 System.out.println("NO");
             }
         }
+       return  buildJsonObject(events);
     }
 
-    private  void processEventRow(Element row) {
+    private JsonArray buildJsonObject(List<Event> events) {
+        JsonBuilderFactory factory = Json.createBuilderFactory(null);
+        JsonArrayBuilder arrayBuilder = factory.createArrayBuilder();
+        //JsonArray value = arrayBuilder
+        for (Event event : events) {
+            arrayBuilder.add(factory.createObjectBuilder().add("date", event.getDate())
+                    .add("description", event.getDescription())
+                    .add("venue", event.getVenue())
+                    .add("venueLink", event.getVenueLink())
+                    .add("documentLink",event.getDocumentLink()));
+        }
+
+   return arrayBuilder.build();
+    }
+
+    private  Event processEventRow(Element row) {
+        Event event = new Event();
         Element venueTD = row.child(0);
+        event.setVenue(venueTD.ownText());
         processVenueRow(venueTD);
         Element dateTD = row.child(1);
+        event.setDate(dateTD.ownText());
         processDateRow(dateTD);
         Element descriptionTD = row.child(2);
+        event.setDescription(descriptionTD.ownText());
         processDescription(descriptionTD);
+        return event;
     }
 
     private void processDescription(Element descriptionTD) {
